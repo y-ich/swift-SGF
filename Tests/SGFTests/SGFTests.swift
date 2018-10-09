88148139df2d7b93990e6ba238ded55683819d26
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2018-10-09 21:39:31 +0900

    test and readme

--- Tests/SGFTests/SGFTests.swift
+++ Tests/SGFTests/SGFTests.swift
@@ -1,26 +1,35 @@
 import XCTest
 @testable import SGF
 
-final class swift_SGFTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        let inputString = "(;FF[4]GN[\\:日本語いけますか？\\]:a]CA[]\n(;B[bb])(;B[cc]))"
-        let tree = try! parseSGF(inputString)
-        XCTAssertNotNil(tree)
+final class SGFUsage: XCTestCase {
+    func testUsageParseSGF() {
+        let inputString = "(;FF[4]GM[1];B[aa](;W[bb])(;W[cc]))"
+        let collection = try! parseSGF(inputString)
+        let root = collection[0]
+        if case let SGFCValueType.single(ffValue) = root.properties["FF"]![0] {
+            XCTAssertEqual(ffValue, "4")
+        } else {
+            XCTFail()
+        }
     }
 
-    func testExample2() {
-        let inputString = "(;RU[Japanese])"
-        let tree = try! parseSGF(inputString)
-        print(tree.description)
-        XCTAssertNotNil(tree)
+    func testUsageSGFEncoder() {
+        let inputString = "(;FF[4]GM[1])"
+        let collection = try! parseSGF(inputString)
+        let str = SGFEncoder.encode(collection: collection)
+        XCTAssertEqual(str, inputString)
     }
+}
 
-    func testExample3() {
-        let inputString = "(;FF[4]GM[1]CA[utf-8]AP[耳赤:119]US[mimiaka1846]SZ[19]RU[Japanese]KM[6.5]DT[2018-06-11]EV[test]GN[test];B[pd];W[dd];B[pp];W[dq];B[do];W[co];B[dp];W[cp];B[eq];W[cn];B[dn])"
-        let tree = try! parseSGF(inputString)
-        XCTAssertNotNil(tree)
+final class SGFTests: XCTestCase {
+    func testParseSGF() {
+        let inputString = "(;FF[4]GN[\\:日本語\\]:a]CA[]\n(;B[bb])(;B[cc]))"
+        let collection = try! parseSGF(inputString)
+        let root = collection[0]
+        if case let SGFCValueType.single(ffValue) = root.properties["FF"]![0] {
+            XCTAssertEqual(ffValue, "4")
+        } else {
+            XCTFail()
+        }
     }
 }
